e00062f8b897f713aa6dde36aa42e3678867d5e6
pmarm <pmarmagne@gmail.com>
2021-09-27 13:31:08 +0200

    fixed env variables

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ import os
 USERNAME        = os.getenv("POSTGRES_USER")#"postgres"
 PASSWORD        = os.getenv("POSTGRES_PASSWORD")#"postgres"
 HOST            = os.getenv("POSTGRES_HOST")#"localhost"
-PORT            = "5432" # os.getenv("POSTGRES_PORT")
-DATABASE_NAME   = "postgres"
+PORT            = os.getenv("POSTGRES_PORT") # os.getenv("POSTGRES_PORT")
+DATABASE_NAME   = os.getenv("PASTGRES_DB")
 
 # Scheme: "postgresql+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 DATABASE_URI = f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE_NAME}"
